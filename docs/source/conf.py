67990114cc4f61b9c0c587f5db4f3eafd5aa3867
Amandus <amandusbutzer@gmx.de>
2016-10-07 16:32:12 +0200

    color switching in progress

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -122,17 +122,23 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-if on_rtd:
-    html_context = {
-        'css_files' : [
-            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-            '_static/custom_color.css',
-        ],
+if not on_rtd:  # only import and set the theme if we're building docs locally   
+    import sphinx_rtd_theme                                                      
+    html_theme = 'sphinx_rtd_theme'                                              
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]                   
+    # Override default css to change color for local build                 
+    def setup(app):                                                              
+        #app.add_javascript("custom.js")                                         
+        app.add_stylesheet('custom_color.css')                                
+else:                                                                            
+    # Override default css to change color for ReadTheDoc build            
+    html_context = {                                                             
+        'css_files': [                                                           
+            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',            
+            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',       
+            '_static/custom_color.css',                                       
+        ],                                                                       
     }
-    html_theme = 'default'
-else:
-    html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
